5a59f86308d059bfe93006ece37723f329f918b9
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2024-01-09 15:51:19 -0500

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
   return (
     <>
-     <h1>Blacksheep Skate Shop</h1>
+      <Header />
+      <main>
+        <p>hello!</p>  
+      </main>
+      <Footer />  
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        header? I hardly know her!
+    </header>
+  )
+}
+
+export default Header
